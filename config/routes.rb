a89863ebb9b4338d746742e827c6eec6b755df67
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2019-02-16 23:28:01 +0100

    ft(deploy): deploy base application on Heroku (#9)

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do 
     namespace :v1 do
-      root to: 'sessions#index'
+      root 'sessions#index'
       post '/users/login', to: 'sessions#create'
       post '/logout', to: 'sessions#destroy' 
       post '/users/signup', to: 'users#create'
